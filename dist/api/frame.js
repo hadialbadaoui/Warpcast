19289e9234a3261a75f98afb0b2f6a092cc452b8
HadiAlBadaoui <hadialbadaoui@gmail.com>
2025-07-24 09:37:00 +0300

    delete api

--- dist/api/frame.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    image: "https://joker4funchallenges.com/image/Joker_logo.png",
-    post_url: "https://joker4funchallenges.com/action",
-    buttons: [{ label: "Play" }]
-  });
-}
