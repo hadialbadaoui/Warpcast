dcdc6ae75a660aaf733bff4ccbe011f674dc8ffb
HadiAlBadaoui <hadialbadaoui@gmail.com>
2025-10-29 11:45:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,53 +14,7 @@ function App() {
     <>
       <div>
 		<h2>Mini App + Vite + TS + React + Wagmi</h2>
-        <ConnectMenu />
       </div>
-    </>
-  );
-}
-
-function ConnectMenu() {
-  const { isConnected, address } = useAccount();
-  const { connect, connectors } = useConnect();
-
-  if (isConnected) {
-    return (
-      <>
-        <div>Connected account:</div>
-        <div>{address}</div>
-        <SignButton />
-      </>
-    );
-  }
-
-  return (
-    <button type="button" onClick={() => connect({ connector: connectors[0] })}>
-      Connect
-    </button>
-  );
-}
-
-function SignButton() {
-  const { signMessage, isPending, data, error } = useSignMessage();
-
-  return (
-    <>
-      <button type="button" onClick={() => signMessage({ message: "hello world" })} disabled={isPending}>
-        {isPending ? "Signing..." : "Sign message"}
-      </button>
-      {data && (
-        <>
-          <div>Signature</div>
-          <div>{data}</div>
-        </>
-      )}
-      {error && (
-        <>
-          <div>Error</div>
-          <div>{error.message}</div>
-        </>
-      )}
     </>
   );
 }
