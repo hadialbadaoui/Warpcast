4460dee404f1f73dcb62b55d103f4986d83e6c01
HadiAlBadaoui <hadialbadaoui@gmail.com>
2025-11-06 17:18:45 +0200

    remove all

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 // src/App.tsx (simplified)
 import { useState } from "react";
-import WelcomeScreen from "./pages/WelcomeScreen";
-import HomeScreen from "./pages/HomeScreen";
 
 export default function App() {
-  const [hasAccess, setHasAccess] = useState(false);
-
-  if (!hasAccess) return <WelcomeScreen onProceed={() => setHasAccess(true)} />;
-  return <HomeScreen />;
+  
 }
